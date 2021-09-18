55a9f5a68fab6ab1eb562429bf07e7b614b56c07
Daniel Luz <dev.gsz@mernen.com>
2021-09-18 15:25:16 -0300

    Improve demo program
    - Support receiving a file name as argument
    - Pretty-print <x>text</x> in a single line
    - Only print inserted tags if they differ from source

--- examples/dump.rs
+++ examples/dump.rs
@@ -1,7 +1,7 @@
 //! A simple utility that outputs the result of some common transforms.
 
 use std::io::Read;
-use std::process;
+use std::{env, process};
 
 use sgmlish::transforms::Transform;
 use sgmlish::{SgmlEvent, SgmlFragment};
@@ -26,8 +26,13 @@ fn run() -> sgmlish::Result<()> {
         })
         .build();
 
-    let mut sgml = String::new();
-    std::io::stdin().read_to_string(&mut sgml).unwrap();
+    let sgml = if let Some(path) = env::args_os().skip(1).next() {
+        std::fs::read_to_string(path).unwrap()
+    } else {
+        let mut buffer = String::new();
+        std::io::stdin().read_to_string(&mut buffer).unwrap();
+        buffer
+    };
 
     let fragment = parser.parse(&sgml)?;
 
@@ -41,13 +46,21 @@ fn run() -> sgmlish::Result<()> {
     }
     println!();
 
-    println!("ℹ️  Attempting to fill end tags:");
-    let fragment = sgmlish::transforms::normalize_end_tags(fragment)?;
-    println!("{}", fragment);
-    println!();
+    let normalized = match sgmlish::transforms::normalize_end_tags(fragment.clone()) {
+        Ok(fragment) => fragment,
+        Err(err) => {
+            eprintln!("Error normalizing end tags:");
+            return Err(err.into());
+        }
+    };
+    if normalized != fragment {
+        println!("ℹ️  Inserting implied end tags:");
+        println!("{}", fragment);
+        println!();
+    }
 
     println!("ℹ️  Pretty-printed:");
-    let fragment = reindent(fragment);
+    let fragment = reindent(normalized);
     println!("{}", fragment);
     println!();
 
@@ -66,28 +79,28 @@ fn reindent(fragment: SgmlFragment) -> SgmlFragment {
         SgmlEvent::Character(s.into())
     }
 
-    for (i, event) in fragment.iter().enumerate() {
-        if i == 0 {
+    let mut keep_same_line = 0;
+    for (i, event) in fragment.iter().enumerate().skip(1) {
+        if keep_same_line > 0 {
+            keep_same_line -= 1;
             continue;
         }
         match event {
             SgmlEvent::OpenStartTag(_)
             | SgmlEvent::Character(_)
             | SgmlEvent::ProcessingInstruction(_)
-            | SgmlEvent::MarkupDeclaration { .. } => {
-                transform.insert_at(i, indent(indent_level));
-            }
-            SgmlEvent::CloseStartTag => indent_level += 1,
+            | SgmlEvent::MarkupDeclaration { .. }
+            | SgmlEvent::MarkedSection { .. } => transform.insert_at(i, indent(indent_level)),
+            SgmlEvent::CloseStartTag => match &fragment.as_slice()[i + 1..] {
+                [SgmlEvent::EndTag(_), ..] => keep_same_line = 1,
+                [SgmlEvent::Character(_), SgmlEvent::EndTag(_), ..] => keep_same_line = 2,
+                _ => indent_level += 1,
+            },
             SgmlEvent::EndTag(_) => {
                 indent_level -= 1;
-                match fragment.as_slice().get(i - 1) {
-                    Some(SgmlEvent::CloseStartTag) => {}
-                    _ => {
-                        transform.insert_at(i, indent(indent_level));
-                    }
-                }
+                transform.insert_at(i, indent(indent_level));
             }
-            _ => {}
+            SgmlEvent::Attribute(..) | SgmlEvent::XmlCloseEmptyElement => {}
         }
     }
 
